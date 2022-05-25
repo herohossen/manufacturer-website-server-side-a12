922de306a3ee99aa1bc4aba009218c2cfcfc1058
herohossen <delowarhossen1224@gmail.com>
2022-05-25 12:04:57 +0600

    Purchase page api added

--- index.js
+++ index.js
@@ -79,7 +79,13 @@ async function run() {
       res.send(tools);
     });
 
-
+    //Tools By Id
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
 
     // post oreder data
     app.post("/tools", async (req, res) => {
