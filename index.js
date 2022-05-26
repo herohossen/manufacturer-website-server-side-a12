f14a2ad7f37c83c34f433d7059eb1e5b618fd996
herohossen <delowarhossen1224@gmail.com>
2022-05-26 16:01:51 +0600

    Order api created

--- index.js
+++ index.js
@@ -52,59 +52,82 @@ async function run() {
       .db("inventory-management-p11")
       .collection("users");
 
-    //User data
-    app.put("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = req.body;
-      const filter = { email: email };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: user,
-      };
-      const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign(
-        { email: email },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "2h" }
-      );
-      res.send({ result, token });
-    });
+      const ordersCollection = client
+        .db("inventory-management-p11")
+        .collection("orders");
+      //User data
+      app.put("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await userCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        const token = jwt.sign(
+          { email: email },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "2h" }
+        );
+        res.send({ result, token });
+      });
 
-    //Tools Section Start
+      //Tools Section Start
 
-    app.get("/tools", async (req, res) => {
-      const query = {};
-      const cursor = toolsCollection.find(query);
-      const tools = await cursor.toArray();
-      res.send(tools);
-    });
+      app.get("/tools", async (req, res) => {
+        const query = {};
+        const cursor = toolsCollection.find(query);
+        const tools = await cursor.toArray();
+        res.send(tools);
+      });
 
-    //Tools By Id
-    app.get("/tools/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const tool = await toolsCollection.findOne(query);
-      res.send(tool);
-    });
+       //Tools By Id
+       app.get("/tools/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const tool = await toolsCollection.findOne(query);
+        res.send(tool);
+      });
 
-    // post oreder data
-    app.post("/tools", async (req, res) => {
-      const newTools = req.body;
-      console.log("adding new Tools", newTools);
-      const result = await toolsCollection.insertOne(newTools);
-      console.log("Add New Tools Result", result);
-      res.send(result);
-    });
+      // // post oreder data
+      // app.post("/tools", async (req, res) => {
+      //   const newTools = req.body;
+      //   console.log("adding new Tools", newTools);
+      //   const result = await toolsCollection.insertOne(newTools);
+      //   console.log("Add New Tools Result", result);
+      //   res.send(result);
+      // });
 
-    //Tools Section End
+      // orders sections start
+      // get order data
+      // change
+      app.get("/order", async (req, res) => {
+        const query = {};
+        const cursor = ordersCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      });
 
-    //get item
-    app.get("/item", async (req, res) => {
-      const query = {};
-      const cursor = itemCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
-    });
+      // app.get("/order/:id", async (req, res) => {
+      //   const id = req.params.id;
+      //   const query = { _id: ObjectId(id) };
+      //   const tool = await ordersCollection.findOne(query);
+      //   res.send(tool);
+      // });
+
+      // post order data
+      app.post("/order", async (req, res) => {
+        const newOrder = req.body;
+        console.log("adding new Tools", newOrder);
+        const result = await ordersCollection.insertOne(newOrder);
+        console.log("Add New order Result", result);
+        res.send(result);
+      });
   } finally {
     // await client.close();
   }
