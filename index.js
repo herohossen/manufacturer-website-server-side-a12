cb5d77ed603e0f71b0119ee35fba47251d917b79
herohossen <delowarhossen1224@gmail.com>
2022-05-26 23:54:04 +0600

    All order api created

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
 // client.connect((err) => {
 //   const collection = client.db('test').collection('devices');
 //   console.log('mongo is running');
@@ -36,8 +35,24 @@ const client = new MongoClient(uri, {
 //   client.close();
 // });
 
-
 // console.log(uri);
+
+//jwt
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 //Funtion for api call
 async function run() {
   try {
@@ -52,85 +67,90 @@ async function run() {
       .db("inventory-management-p11")
       .collection("users");
 
-      const ordersCollection = client
-        .db("inventory-management-p11")
-        .collection("orders");
-      //User data
-      app.put("/user/:email", async (req, res) => {
-        const email = req.params.email;
-        const user = req.body;
-        const filter = { email: email };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: user,
-        };
-        const result = await userCollection.updateOne(
-          filter,
-          updateDoc,
-          options
-        );
-        const token = jwt.sign(
-          { email: email },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: "2h" }
-        );
-        res.send({ result, token });
-      });
+    const ordersCollection = client
+      .db("inventory-management-p11")
+      .collection("orders");
+    //User data
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "2h" }
+      );
+      res.send({ result, token });
+    });
 
-      //Tools Section Start
+    //Tools Section Start
 
-      app.get("/tools", async (req, res) => {
-        const query = {};
-        const cursor = toolsCollection.find(query);
-        const tools = await cursor.toArray();
-        res.send(tools);
-      });
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
-       //Tools By Id
-       app.get("/tools/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const tool = await toolsCollection.findOne(query);
-        res.send(tool);
-      });
+    //Tools By Id
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
 
-      // // post oreder data
-      // app.post("/tools", async (req, res) => {
-      //   const newTools = req.body;
-      //   console.log("adding new Tools", newTools);
-      //   const result = await toolsCollection.insertOne(newTools);
-      //   console.log("Add New Tools Result", result);
-      //   res.send(result);
-      // });
+    // // post oreder data
+    // app.post("/tools", async (req, res) => {
+    //   const newTools = req.body;
+    //   console.log("adding new Tools", newTools);
+    //   const result = await toolsCollection.insertOne(newTools);
+    //   console.log("Add New Tools Result", result);
+    //   res.send(result);
+    // });
 
-      // orders sections start
-      // get order data
-      // change
-      app.get("/order", async (req, res) => {
-        const email =req.query.email;
-        const query = {email: email};
+    // orders sections start
+
+    app.get("/order", async (req, res) => {
+      const query = {};
+      const cursor = ordersCollection.find(query);
+      const allorders = await cursor.toArray();
+      res.send(allorders);
+    });
+
+
+    app.get("/order", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      // const authorization = req.headers.authorization;
+      // console.log(authorization);
+      const decodedEmail = req.decoded.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
         // const cursor = ordersCollection.find(query);
         // const orders = await cursor.toArray();
         const orders = await ordersCollection.find(query).toArray();
-        res.send(orders);
-        console.log(orders);
-      });
+        return res.send(orders);
+        // console.log(orders);
+        return res.status(403).send({ message: "forbidden access" });
+      } else {
+      }
+    });
 
-      // app.get("/order/:id", async (req, res) => {
-      //   const id = req.params.id;
-      //   const query = { _id: ObjectId(id) };
-      //   const tool = await ordersCollection.findOne(query);
-      //   res.send(tool);
-      // });
 
-      // post order data
-      app.post("/order", async (req, res) => {
-        const newOrder = req.body;
-        console.log("adding new Tools", newOrder);
-        const result = await ordersCollection.insertOne(newOrder);
-        console.log("Add New order Result", result);
-        res.send(result);
-      });
+
+    // post order data
+    app.post("/order", async (req, res) => {
+      const newOrder = req.body;
+      console.log("adding new Tools", newOrder);
+      const result = await ordersCollection.insertOne(newOrder);
+      console.log("Add New order Result", result);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
